e7497f0fd3779986071ec02535c6d538c38350d9
Thiago Rodrigues Santos <trsantos1@gmail.com>
2014-12-15 12:27:57 -0300

    Exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,13 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
