c5628c85d223accf7a9bb07a6a6553a66fe707a5
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-01-05 06:56:38 -0300

    Add interval to update feeds

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,19 +3,31 @@ class Feed < ActiveRecord::Base
   validates :feed_url, presence: true, uniqueness: true
 
   def update
+    # I don't now if I should make this a controller action...
+
+    # wait 1 hour between updates
+    return if self.updated_at > 1.hour.ago and self.entries.count > 0
+
     fj_feed = Feedjira::Feed.fetch_and_parse self.feed_url
-    if fj_feed.is_a? Integer
-      return
-    end
-    entries = fj_feed.entries
+
+    # stop if feed coudn't be fetched
+    return if fj_feed.is_a? Integer
+
+    # update entries
+    # entries = fj_feed.entries
+    entries = fj_feed.entries.sort_by { |e| e.published }.reverse
     self.entries.destroy_all
     4.times do |n|
       if entries[n]
         self.entries.create(title:       entries[n].title,
-                            description: entries[n].content || entries[n].summary,
+                            description: entries[n].content   || entries[n].summary,
                             pub_date:    entries[n].published || Time.zone.now,
                             url:         entries[n].url)
       end
     end
+
+    # mark feed as updated
+    self.updated_at = Time.zone.now
+    self.save
   end
 end
