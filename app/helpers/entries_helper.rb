69e6e9e482635ff9079cc19badfc3d911bf6635a
Thiago Rodrigues <trsantos1@gmail.com>
2015-04-14 17:10:28 -0300

    Experiment with shading all the contents from entries already seen

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,7 +2,7 @@ module EntriesHelper
   def old_entry? entry
     if logged_in?
       s = current_user.subscriptions.find_by(feed_id: entry.feed_id)
-      if !s.nil? and s.visited_at > entry.pub_date
+      if !s.nil? and !s.visited_at.nil? and s.visited_at > entry.pub_date
         return true
       end
     end
