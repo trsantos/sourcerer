b6b86242c40a5c0b9af9f4660f4612c88717e2bf
Thiago Rodrigues <trsantos1@gmail.com>
2015-05-27 09:49:16 -0300

    Forgot to add NextFeed files

---
+++ app/models/next_feed.rb
@@ -0,0 +1,2 @@
+class NextFeed < ActiveRecord::Base
+end
---
+++ test/models/next_feed_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NextFeedTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
