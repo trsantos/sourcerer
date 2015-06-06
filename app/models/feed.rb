5bafb468178f2cd23635e02c7191e29acaf68ace
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-06 14:38:12 -0300

    Don't insert entries whose titles are already in the database

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -48,7 +48,7 @@ class Feed < ActiveRecord::Base
     updated = false
 
     feed.entries.first(5).each do |e|
-      unless self.entries.find_by(url: e.url)
+      unless self.entries.find_by(url: e.url) or self.entries.find_by(title: e.title)
         updated = true
         insert_entry e
       end
