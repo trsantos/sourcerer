b050e0f7821c0a04315ab43d931d47ccfee0e597
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-06 02:31:38 -0300

    We should not care about an entry being old or not

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,5 +1,7 @@
 module EntriesHelper
   def old_entry? entry
+    # Disable this old entries stuff for now
+    return false
     if logged_in?
       s = current_user.subscriptions.find_by(feed_id: entry.feed_id)
       if !s.nil? and !s.visited_at.nil? and s.visited_at > entry.pub_date
