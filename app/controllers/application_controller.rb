85ed483da9f79eaeb5e7e98d12c8a8c27e4d1283
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-10 18:28:48 -0300

    Never mind profiler on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,6 @@
 class ApplicationController < ActionController::Base
-  require 'open-uri'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  before_filter :authorize
-
-  private
-
-  def authorize
-    if current_user.admin?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
-
 end
