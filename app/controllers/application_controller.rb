53f025f68e6408833691f2301518f8a09a172cd8
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-10 18:12:32 -0300

    Try again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :authorize
+  before_filter :authorize
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
-
-4.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
-
-Feed.create!(title: "Example Feed",
-             feed_url: "http://feed.rss",
-             site_url: "http://www.rubyonrails.org")
-feed = Feed.first
-5.times do
-  description = Faker::Lorem.sentence(30)
-  title = Faker::Lorem.sentence(5)
-  feed.entries.create!(title: title,
-                       description: description,
-                       pub_date: Time.zone.now)
-end
