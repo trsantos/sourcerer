a1ffb56dcebd54a4ec2e35f5f8c591b646cf9452
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-20 00:45:56 -0300

    lololol

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,6 @@
 module EntriesHelper
   def old? entry
     s = current_user.subscriptions.find_by(feed_id: entry.feed_id)
-    return !s.nil? && !s.visited_at.nil? && s.visited_at > entry.created_at
+    return !s.nil? && !s.visited_at.nil? && s.visited_at > entry.pub_date
   end
 end
