0ddabf765710050adc790f4d18dc5dac52a8fab0
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-22 19:11:53 -0300

    Let's see if we fix HN and 500px

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,8 @@
 module EntriesHelper
   def old? entry
     s = current_user.subscriptions.find_by(feed_id: entry.feed_id)
-    return !s.nil? && !s.visited_at.nil? && s.visited_at > entry.pub_date
+    return !s.nil? &&
+           !s.visited_at.nil? &&
+           s.visited_at > entry.created_at
   end
 end
