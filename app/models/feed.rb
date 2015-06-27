64a08d0968470ced2c03d76b3ceb780e5f19adf6
Thiago Rodrigues <trsantos1@gmail.com>
2015-06-27 01:49:30 -0300

    Try to simplify image fetching.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -61,7 +61,8 @@ class Feed < ActiveRecord::Base
   def setup_fj
     Feedjira::Feed.add_common_feed_entry_element("enclosure", :value => :url, :as => :image)
     Feedjira::Feed.add_common_feed_entry_element("media:thumbnail", :value => :url, :as => :image)
-    Feedjira::Feed.add_common_feed_entry_element("media:content", :value => :url, :as => :image)
+    Feedjira::Feed.add_common_feed_entry_element("img", :value => :src, :as => :image)
+    #Feedjira::Feed.add_common_feed_entry_element("media:content", :value => :url, :as => :image)
   end
 
   def insert_entry(e)
@@ -91,21 +92,19 @@ class Feed < ActiveRecord::Base
       return nil
     end
 
-    if img.start_with? '//'
-      img = "http:" + img
-    elsif img.start_with? '/'
-      parse = URI.parse(self.site_url || self.feed_url)
-      img = parse.scheme + '://' + parse.host + img
-    elsif img.start_with? '../'
-      parse = URI.parse(self.site_url || self.feed_url)
-      img = parse.scheme + '://' + parse.host + img[2..-1]
-    elsif !img.start_with? 'http'
-      parse = URI.parse(self.site_url || self.feed_url)
-      # parse.path is bogus if site_url doesn't exist
-      img = parse.scheme + '://' + parse.host + parse.path + img
-    end
+    # maybe not needed anymore... let's see
+    # uri = URI.uri(self.site_url || self.feed_url)
+    # if img.start_with? '//'
+    #   img = "http:" + img
+    # elsif img.start_with? '/'
+    #   img = uri.scheme + '://' + uri.host + img
+    # elsif img.start_with? '../'
+    #   img = uri.scheme + '://' + uri.host + img[2..-1]
+    # elsif !img.start_with? 'http'
+    #   img = uri.scheme + '://' + uri.host + uri.path + img
+    # end
 
-    return filter_image img
+    return img
   end
 
   def find_image_from_description(description)
@@ -137,57 +136,7 @@ class Feed < ActiveRecord::Base
       img += '_896x504.jpg'
     end
 
-    # discard silly images
-    if img.include? 'feedburner' or
-      # img.include? 'pml.png' or
-      # img.include? 'rc.img' or
-      # img.include? 'mf.gif' or
-      # img.include? 'ptq.gif' or
-      # img.include? 'twitter16.png' or
-      # img.include? 'sethsblog' or
-      # img.include? 'assets.feedblitz.com/i/' or
-      # img.include? 'wirecutter-deals' or
-      # img.include? '/heads/' or
-      # img.include? '/share/' or
-      # img.include? 'smile.png' or
-      # img.include? 'blank.' or
-      # img.include? 'application-pdf.png' or
-      # img.include? 's-US_UK_CA-mini' or
-      # img.include? 'gif;base64' or
-      # img.include? 'abrirpdf.png' or
-      # img.include? 'gravatar.com/avatar' or
-      # img.include? 'nojs.php' or
-      # img.include? 'icon' or
-      # img.include? 'gplus-16.png' or
-      # img.include? 'logo' or
-      # img.include? 'avw.php' or
-      # img.include? 'service_links' or
-      # img.include? 'tmn-test' or
-      # img.include? '-ipad-h' or
-      # img.include? 'webkit-fake-url' or
-      # img.include? '/img/oglobo.jpg' or
-      # img.include? 'beacon' or
-      # img.include? 'usatoday-newstopstories' or
-      # img.include? 'a2.img' or
-      # img.include? 'ach.img' or
-      # img.include? '/comments/' or
-      # img.include? '/smilies/' or
-      # img.include? 'simple-share-buttons-adder' or
-      # img.include? 'a2t.img' or
-      # img.include? 'a2t2.img' or
-      # img.include? 'default-thumbnail' or
-      # img.include? 'subscribe' or
-      # img.include? 'forbes_' or
-      # img.include? 'emoji' or
-      # img.include? 'transparent.png' or
-      # img.include? 'cdh_rss' or
-      # img.include? 'ynp-rss' or
-      # Disable the next to filters when og images are not used
-      # img.include? 'bbcimg.co.uk' or
-      # img.include? '/images/facebook' or
-      # img.include? 'phys.org/newman/csz/news/tmb' or
-      # img.include? 'uol-jogos-600px' or
-      img.include? '.tiff' or
+    if img.include? '.tiff' or
       img.include? '.mp3' or
       img.include? '.m4a' or
       img.include? '.mp4' or
