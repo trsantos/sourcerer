b320c1f6aa81b82d467dd40658992457f79182b6
Thiago Rodrigues <trsantos1@gmail.com>
2015-07-03 16:47:41 -0300

    30 entries in River

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -63,7 +63,8 @@ class SubscriptionsController < ApplicationController
     # @entries = []
     # current_user.feeds.each { |f| @entries += f.entries.order(pub_date: :desc).first 2 }
     # @entries = @entries.sort_by{ |e| e.pub_date }.reverse!.first(30)
-    @entries = current_user.entries.order(pub_date: :desc).first(20)
+    # @entries = current_user.entries.order(pub_date: :desc).group(:feed_id).limit(2)
+    @entries = current_user.entries.order(pub_date: :desc).first(30)
     @only_images = false
     @displaying_river = true
   end
