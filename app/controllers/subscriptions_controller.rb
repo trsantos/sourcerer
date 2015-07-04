08dfd06b830bdfef4d6623128accdf7a60615b7b
Thiago Rodrigues <trsantos1@gmail.com>
2015-07-03 21:02:32 -0300

    No filtering.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -60,8 +60,9 @@ class SubscriptionsController < ApplicationController
   end
 
   def river
-    @entries = current_user.entries.select('DISTINCT ON (entries.pub_date, entries.feed_id) *').order(pub_date: :desc, feed_id: :asc).group(:feed_id, :id, 'feeds.id', 'subscriptions.id').first(10)
+    # @entries = current_user.entries.select('DISTINCT ON (entries.pub_date, entries.feed_id) *').order(pub_date: :desc, feed_id: :asc).group(:feed_id, :id, 'feeds.id', 'subscriptions.id').first(10)
     # @entries = current_user.entries.order(pub_date: :desc).group(:feed_id).first(10)
+    @entries = current_user.entries.order(pub_date: :desc).first(10)
     @only_images = false
     @displaying_river = true
   end
