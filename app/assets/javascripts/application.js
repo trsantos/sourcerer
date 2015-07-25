4b53cb10a81acae1e5f892acdce94901a908f1a0
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-07-25 19:53:33 -0300

    fix margins. fix fh hover color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,6 @@
 
 $(function(){
     Turbolinks.enableProgressBar();
-	$(document).on("page:before-change", function () { $(window).scrollTop(0) });
+//	$(document).on("page:before-change", function () { $(window).scrollTop(0) });
 	$(document).foundation({ topbar: { scrolltop: false } });
 });
