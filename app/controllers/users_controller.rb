f0c34300381b2ca3c340950aafa3211fe1cfc211
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-08-25 19:26:38 -0300

    update to user account settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,12 +33,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = 'Profile updated.'
-      redirect_to @user
-    else
-      render 'edit'
-    end
+    flash[:success] = 'Profile updated.' if @user.update_attributes(user_params)
+    render 'edit'
   end
 
   def destroy
