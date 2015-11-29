28331227fa33c12db678b5bce12d3d28d55018eb
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-11-29 17:06:07 -0200

    visual clue for old entries

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,10 @@
 module EntriesHelper
+  def old?(entry)
+    sub = current_user.subscriptions.find_by(feed_id: entry.feed.id)
+    return true if entry.pub_date < sub.visited_at
+    return true if entry.created_at < sub.visited_at
+    false
+  rescue
+    false
+  end
 end
