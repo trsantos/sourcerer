123ac0d2bab8530c464ecb92b940d143f2c4b2da
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-12-04 11:48:19 -0200

    disable dj on web dyno

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,6 @@ port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  @delayed_job ||= spawn('bundle exec bin/delayed_job -n 4 start')
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
