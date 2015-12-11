fe033359960fca3f64b085dc05fbce99793b805e
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-12-11 12:13:07 -0300

    freeze literals inside application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = 'Sourcerer'
+    base_title = 'Sourcerer'.freeze
     if page_title.empty?
       base_title
     else
@@ -9,19 +9,21 @@ module ApplicationHelper
   end
 
   def in_feeds_show
-    params[:controller] == 'feeds' && params[:action] == 'show'
+    params[:controller] == 'feeds'.freeze && params[:action] == 'show'.freeze
   end
 
   def in_auth?
-    (params[:controller] == 'users' && params[:action] == 'new') ||
-      (params[:controller] == 'sessions' && params[:action] == 'new')
+    (params[:controller] == 'users'.freeze &&
+     params[:action] == 'new'.freeze) ||
+      (params[:controller] == 'sessions'.freeze &&
+       params[:action] == 'new'.freeze)
   end
 
   def process_url(url)
     return nil if url.blank?
     url = url.strip
-    unless url.start_with?('http:') || url.start_with?('https:')
-      url = 'http://' + url
+    unless url.start_with?('http:'.freeze) || url.start_with?('https:'.freeze)
+      url = 'http://'.freeze + url
     end
     url
   end
