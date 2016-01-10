d4352046b2ea14e466f3a2c94176f7e2982d9684
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-01-10 10:44:19 -0300

    Remove tests

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title,		"Reader"
-    assert_equal full_title("Help"),	"Help | Reader"
-  end
-end
