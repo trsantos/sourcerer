44ff431b3f5cd521f1ee38a8f455b68615b58a6a
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-01-10 16:34:55 -0300

    Take steps to upgrade the app from Rails 4.1 to 4.2
    I should have done this a long time ago. Nothing really changed tough, so everything should be fine.

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,4 +5,3 @@ Rails.backtrace_cleaner.add_silencer { |line| line =~ /rbenv/ }
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
-
