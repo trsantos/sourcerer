f3eb0ec389bfbb49e809d0244104f568dd70f622
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-01-13 02:36:07 -0300

    Simplify loading of Foundation JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,29 +13,12 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require foundation/foundation
-//= require foundation/foundation.alert
-//= require foundation/foundation.topbar
-// require foundation/foundation.abide
-// require foundation/foundation.accordion
-// require foundation/foundation.clearing
-// require foundation/foundation.dropdown
-// require foundation/foundation.equalizer
-// require foundation/foundation.interchange
-// require foundation/foundation.joyride
-// require foundation/foundation.magellan
-// require foundation/foundation.offcanvas
-// require foundation/foundation.orbit
-// require foundation/foundation.reveal
-// require foundation/foundation.slider
-// require foundation/foundation.tab
-// require foundation/foundation.tooltip
+//= require foundation
 //= require turbolinks
 //= require_tree .
 
 $(function(){
     Turbolinks.enableProgressBar();
     Turbolinks.pagesCached(0);
-    $(document).on("page:before-change", function () { $(window).scrollTop(0) });
-    $(document).foundation({ topbar: { scrolltop: true } });
+    $(document).foundation();
 });
