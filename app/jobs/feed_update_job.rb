d0b01aede9ea6161f13502c1013c830bc2b9f80f
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-08-09 22:03:13 -0300

    Fail silently if we cannot update a feed

--- app/jobs/feed_update_job.rb
+++ app/jobs/feed_update_job.rb
@@ -4,5 +4,8 @@ class FeedUpdateJob < ApplicationJob
   def perform(feed)
     feed.update
     ActiveRecord::Base.connection.close
+  rescue
+    # TODO: log an error message if the update fails
+    nil
   end
 end
