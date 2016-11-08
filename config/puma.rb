9298f28e8fbbf65ded7da0e550845fa3368f0ff7
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-11-08 22:19:50 +0000

    Readd bindings to config/puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind 'tcp://45.63.94.128:80'
+bind 'ssl://45.63.94.128:443/?key=/usr/local/etc/letsencrypt/live/sourcerer.news/privkey.pem&cert=/usr/local/etc/letsencrypt/live/sourcerer.news/fullchain.pem'
