1b9170958e55d37fa469cb258a9b235fa7f0f176
Thiago Rodrigues Santos <trsantos1@gmail.com>
2016-11-09 20:31:57 -0200

    Change app domain to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     authentication:        :plain,
     user_name:             ENV['SENDGRID_USERNAME'],
     password:              ENV['SENDGRID_PASSWORD'],
-    domain:                'heroku.com',
+    domain:                'sourcerer.news',
     enable_starttls_auto:  true
   }
 end
