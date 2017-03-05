2d6b05d08d3a5b9d97b922b7433596f96c202716
Thiago Rodrigues Santos <trsantos1@gmail.com>
2017-03-05 20:06:02 -0300

    Allow adding of Youtube feed using normal channel url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,19 @@ module ApplicationHelper
     return nil if url.blank?
     url = url.strip
     url = 'http://' + url unless url.start_with?('http:', 'https:')
+    if url.include?('youtube.com/user') || url.include?('youtube.com/channel')
+      get_youtube_feed url
+    else
+      url
+    end
+  end
+
+  def get_youtube_feed(url)
+    require 'open-uri'
+    doc = Nokogiri::HTML(open(url))
+    id = doc.css("meta[itemprop='channelId']").first.attributes['content'].value
+    'https://www.youtube.com/feeds/videos.xml?channel_id=' + id
+  rescue
     url
   end
 end
