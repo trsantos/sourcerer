c3563faf116d96ca123a7626b4ca6e83050e35bf
Thiago Rodrigues Santos <thiago@fastmail.com>
2017-09-10 09:20:44 -0300

    Update to Rails 5.1
    Signed-off-by: Thiago Rodrigues Santos <thiago@fastmail.com>

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Reader
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
     config.action_mailer.default_url_options = { host: 'www.sourcerer.news' }
     config.active_job.queue_adapter = :sidekiq
   end
