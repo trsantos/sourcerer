e19ae7c4f22b297cf1133fdb83bfbd35b194d1c9
Thiago Rodrigues Santos <thiago@fastmail.com>
2017-09-10 08:39:03 -0300

    Move production puma configuration into its own file
    Signed-off-by: Thiago Rodrigues Santos <thiago@fastmail.com>

---
+++ config/puma/production.rb
@@ -0,0 +1,2 @@
+bind 'tcp://45.63.94.128:80'
+bind 'ssl://45.63.94.128:443/?key=/usr/local/etc/letsencrypt/live/sourcerer.news/privkey.pem&cert=/usr/local/etc/letsencrypt/live/sourcerer.news/fullchain.pem'
