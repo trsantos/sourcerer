35fc9ba257bfd4065a19a6816babf16f0e1bc2fa
Thiago Rodrigues Santos <thiago@fastmail.com>
2017-09-10 21:44:04 -0300

    Show feedback page only for logged in users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     redirect_to current_user.next_feed if logged_in?
   end
 
-  # maybe require login here?
   def feedback
+    redirect_to root_url unless logged_in?
   end
 end
